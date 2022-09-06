1d0a8203c315cc3a7345ceb926ced768715dc392
CNOV0 <maya.zygadlo@yahoo.fr>
2022-09-06 17:51:39 +0200

    Ajout du code pour utiliser NACCESS

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
-import Bio
+from Bio.PDB.PDBParser import PDBParser
+par = PDBParser()
+structure = par.get_structure('2K1A','Projet_Membrane_Maya/data/2k1a.pdb')
+import Bio.PDB.NACCESS as nac  # module utiliser pour trouver les molecules accessibles au solvant
 
-Bio.PDB.NACCESS  # module utiliser pour trouver les molecules accessibles au solvant
+tmp_dir = "Projet_Membrane_Maya/results/tmp/"
+Bio.PDB.NACCESS.run_naccess(model=structure[0], \
+                            pdb_file="Projet_Membrane_Maya/data/2k1a.pdb", \
+                            temp_path=tmp_dir)
