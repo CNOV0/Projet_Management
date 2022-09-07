c19f4c1fb666a3d866620c55425901ad345cf408
CNOV0 <maya.zygadlo@yahoo.fr>
2022-09-07 22:17:01 +0200

    Completion find_CA et calculate_COM.

--- src/main.py
+++ src/main.py
@@ -19,9 +19,27 @@ def access_solvant():
 
 def find_CA(file_pdb):
 	# trouve CA Puis selection de ceux qui sont en accord avec acces select
+	CA = []
 	with open(file_pdb, "r") as file_in:
 		for ligne in file_in:
-			piou = 0
+			if "ATOM" in ligne and "CA" in ligne:
+				CA.append(ligne)
+	data_ca = []
+	for i in range(len(CA)):
+		# coupe liste[i] selon les espaces et recup le n eme mot que l'on converti
+		data = {"nb_ca" : int(CA[i].split()[5]), "x_ca": int(CA[i].split()[6]), \
+				"y_ca": int(CA[i].split()[7]), "z_ca": int(CA[i].split()[8])}
+		data_ca.append(data)
+	return data_ca
+
+
+def calculate_COM():
+	# le vecteur position du COM d'un ensemble de solide vaut
+	# la moyenne pondérée par les valeurs des masses des vect positions des solides de l'ensemble
+	# Le COM : COM = (sum(miri) / M)
+	# ou M est la somme des masses de particules; N est le nb de masses m;
+	# ri est la distance du point de ref
+	return 0
 
 
 if __name__=="__main__":
