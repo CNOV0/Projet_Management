b09c806797ebd133e367a6ac476b543d0eaa39c2
CNOV0 <maya.zygadlo@yahoo.fr>
2022-09-07 15:38:10 +0200

    Ajout des modules sys et os

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
+import os
+import sys
 from Bio.PDB import PDBParser
 from Bio.PDB.DSSP import DSSP
-p = PDBParser()
-structure = p.get_structure("2K1A", "Projet_Membrane_Maya/data/2k1a.pdb")
-model = structure[0]
-dssp = DSSP(model, "Projet_Membrane_Maya/data/2k1a.pdb")
 
-a_key = list(dssp.keys())[1]
-dssp[a_key]
+def dssp(file_pdb):
+    parsed = PDBParser()
+    structure = parsed.get_structure("2K1A", file_pdb)
+    model = structure[0]
+    data_dssp = DSSP(model, file_pdb)
+    return data_dssp
+
+
+if __name__=="__main__":
+	if len(sys.argv) != 2:
+		sys.exit("Erreur : Il faut donner un seul argument qui est le nom du fichier pdb.")
+
+	if not os.path.exists(sys.argv[1]):
+		sys.exit("Erreur : Le fichier donné n'existe pas.")
+	
+	filename = sys.argv[1]
+	data_dssp = dssp(filename)
+	a_key = list(data_dssp.keys())[1]
+	print(data_dssp[a_key])
+
