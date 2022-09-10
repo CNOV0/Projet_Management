c8ea8a93be688fa7a4a02912c05a43593aefc209
CNOV0 <mayazygadlo@DESKTOP-TARTU7S.localdomain>
2022-09-10 10:22:44 +0200

    Ajout de docstring.

--- src/main.py
+++ src/main.py
@@ -6,20 +6,71 @@ from Bio.PDB import PDBParser
 from Bio.PDB.DSSP import DSSP
 
 def dssp(file_pdb):
+	"""Run DSSP on a PDB file
+
+	Used to get the solvant accessibility of each atom.
+
+	Parameters
+	----------
+	file_pdb : str
+		The name of a PDB file
+
+		The filename may contain de path to fetch the file.
+
+	Returns
+	-------
+	list ?
+		Different informations on the atom
+		- especially the accessibility.
+	"""
     parsed = PDBParser()
     structure = parsed.get_structure("2K1A", file_pdb)
     model = structure[0]
-    data_dssp = DSSP(model, file_pdb)
+    dssp_data = DSSP(model, file_pdb)
     
-    return data_dssp
+    return dssp_data
 
 
-def access_solvant(data_dssp, index):
-	a_key = list(data_dssp.keys())[index]
-	return data_dssp[a_key][3]
+def access_solvant(dssp_data, index):
+	"""Get the value of accessibility for a given atom
+
+	Parameters
+	----------
+	dssp_data : list ?
+		A list containing various informations and the accessibility
+	index : int
+		The atom for which we want the accessibility
+
+	Returns
+	-------
+	int
+		The solvant accessibility of the atom
+	"""
+	a_key = list(dssp_data.keys())[index]
+	return dssp_data[a_key][3]
 
 
-def find_CA_access(data_dssp, file_pdb):
+def find_CA_access(dssp_data, file_pdb):
+	"""Find the alpha carbon accessible to the solvant
+
+	Find the alpha carbon in a PDB file
+	and select only those who are accessible to the solvant
+
+	Parameters
+	----------
+	dssp_data : list ?
+		A list containing various informations and the accessibility
+	file_pdb : str
+		The name of a PDB file
+
+		The filename may contain de path to fetch the file.
+
+	Returns
+	-------
+	Pandas DataFrame
+		DataFrame containing the solvant accessibility, the residu's name,
+		the coordinates of the alpha carbon.
+	"""
 	CA = []
 	
 	with open(file_pdb, "r") as file_in:
@@ -32,7 +83,7 @@ def find_CA_access(data_dssp, file_pdb):
 	ca_access = []
 	
 	for i in range(len(CA)):
-		access = access_solvant(data_dssp, i)
+		access = access_solvant(dssp_data, i)
 		if access > 0.5:
 			data = [access, CA[i].split()[3], \
 					float(CA[i].split()[6]), \
@@ -40,10 +91,26 @@ def find_CA_access(data_dssp, file_pdb):
 					float(CA[i].split()[8])]
 			ca_access.append(data)
 	
-	return pd.DataFrame(ca_access, columns=["accessibility", "nom_resid", "x", "y", "z"])
+	return pd.DataFrame(ca_access, columns=["accessibility", "resid_name", "x", "y", "z"])
 
 
 def calculate_COM(ca_data):
+	"""Calculate the center of mass of a protein
+
+	Parameters
+	----------
+	ca_data : Pandas DataFrame
+		DataFrame containing the atoming coordinates of an alpha carbon
+        
+		The DataFrame contains also other informations such as
+		the solvant accessibility and the residu's name.
+
+	Returns
+	-------
+	list
+        List containing the center of mass of a
+        protein for each atomic coordinate
+	"""
 	x_COM = ca_data["x"].mean()
 	y_COM = ca_data["y"].mean()
 	z_COM = ca_data["z"].mean()
@@ -52,12 +119,44 @@ def calculate_COM(ca_data):
 
 
 def center_protein(COM, ca_data):
+	"""Center a protein in (0, 0, 0)
+
+	Parameters
+	----------
+	COM: list
+		List containing the atomic coordinates of
+		the center of mass of a protein
+	ca_data : Pandas DataFrame
+		DataFrame containing the coordinates of the alpha carbon of a protein
+
+		The DataFrame contains also other information such as
+		the solvant accessibility of the atom and the residu's name.
+
+	Returns
+	-------
+	int
+		Le produit des deux nombres.
+	"""
 	ca_data["x"] -= COM[0]
 	ca_data["y"] -= COM[1]
 	ca_data["z"] -= COM[2]
 
 
 def fibonacci_sphere(samples=1000):
+	"""Generate uniforme distribution of points on a hemisphere
+
+	This function was taken from the internet.
+
+	Parameters
+	----------
+	samples : int
+		The number of points wanted on the hemisphere
+
+	Returns
+	-------
+	list of tuple
+		The coordinates of each point on the hemisphere
+	"""
 	points = []
 	phi = math.pi * (3. - math.sqrt(5.))  # golden angle in radians
 
@@ -75,28 +174,31 @@ def fibonacci_sphere(samples=1000):
 	return points
 
 
-def atom_is_transmembrane(sphere_points, ca_coords):
-	membrane_width = 22  # Dans le cas d'une protein ayant just eun squelette CA
-	point_min = [0, 0, 0]  # valeur a mettre (xmin,ymin,zmin)
-	point_max = [0, 0, 0]
-	# boucle sur chaque point sphere et check si est dans plane
+def calculate_hydrobicity(sphere_points, ca_coords):
+	membrane_width = 22  # For protein with just CA - 15 A otherwise
+	point_min = [ca_coords["x"].min(), ca_coords["y"].min(), ca_coords["z"].min()]
+	point_max = [ca_coords["x"].max(), ca_coords["y"].max(), ca_coords["z"].max()]
+	# Loop on each point of the sphere
 	for point in sphere_points:
-		# ici paraleliser - itere sur min -> max par saut de 1A
+		# should be parallelised
 		while point_min < point_max:
-			if carbon_is_in_membrane():
-				hydro = 0
-				# check si hydrophobe
-				# calcul hydrophobicité
-				# garder best one w/ info pour la retrouvé
-			point_min = 0  # new mean point (ajout d'1A)
+			for ligne in ca_coords:  # pas sure
+				nb_ca, hydro = 0, 0
+				if carbon_is_in_membrane(point, ARG):
+					nb_ca += 1
+					if ca_coords.loc[INDICE, "resid_name"] is in [HYDROPHOBES]:
+						hydro += 1
+			hydro /= nb_ca
+			#  keep best one w/ info to locate it
+			point_min = 0  # new mean point (add 1A)
 	return hydrophobicity
 
 
 def carbon_is_in_membrane():
-	d_plane1 = calcule_plane()
-	d_plane2 = calculate_plane()
-	d_ca = calculate_plane()  # sphere_points[0] * atom.x + sphere_points[1] * atom.y + sphere_points[2] * atom.z
-	if (d_ca > d_plane1) and (d_ca < d_plane2):
+	plane1_d = calcule_plane()
+	plane2_d = calculate_plane()
+	ca_d = calculate_plane()  # sphere_points[0] * atom.x + sphere_points[1] * atom.y + sphere_points[2] * atom.z
+	if (ca_d > plane1_d) and (ca_d < plane2_d):
 		return True
 	else:
 		return False
@@ -105,10 +207,10 @@ def carbon_is_in_membrane():
 def calculate_plane():
 	x_0 = ca_data.loc[0, "x"]
 	# calcule l'équation de plan
-	d_plan = sphere_points[0] * (min_point[0] + sphere_points[0] * membraine_width) + \
+	plane_d = sphere_points[0] * (min_point[0] + sphere_points[0] * membraine_width) + \
 			sphere_points[1] * (min_point[1] + sphere_point[1] * membraine_width) + \
 			sphere_points[2] * (min_point[2] + sphere_points[2] * membraine_width)
-	return d_plan
+	return plane_d
 
 
 if __name__=="__main__":
@@ -120,7 +222,7 @@ if __name__=="__main__":
 	
 	filename = sys.argv[1]
 	
-	data_dssp = dssp(filename)
+	dssp_data = dssp(filename)
 	ca_access = find_CA_access(data_dssp, filename)
 	COM = calculate_COM(ca_access)
 	center_protein(COM, ca_access)
