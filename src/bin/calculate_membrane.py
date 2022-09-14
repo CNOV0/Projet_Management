b388f69d4c051ffa18c33ed721be19b45ceabd7f
CNOV0 <maya.zygadlo@yahoo.fr>
2022-09-14 13:59:55 +0200

    Change le nom d'une fonction.

--- src/bin/calculate_membrane.py
+++ src/bin/calculate_membrane.py
@@ -68,7 +68,7 @@ def position(sphere_points, ca_data):
         while mini["y"] < maxi["y"]:
             nb_ca, hydro = 0, 0
             for i in range(ca_data.shape[0]):  # loop on the CA
-                if carbon_is_in_membrane(point, ca_data.iloc[i], mini, 22):
+                if is_in_membrane(point, ca_data.iloc[i], mini, 22):
                     nb_ca += 1
                     if is_hydrophobe(ca_data.iloc[i]):
                         hydro += 1
@@ -105,7 +105,7 @@ def is_hydrophobe(carbon):
     return False
 
 
-def carbon_is_in_membrane(sphere_pt, ca_coords, point_min, memb_width):
+def is_in_membrane(sphere_pt, ca_coords, point_min, memb_width):
     """Test if an atom is between two planes.
 
     Parameters
